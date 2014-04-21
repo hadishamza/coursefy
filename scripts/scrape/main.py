100f6ad72947aa25e87a5efeb65de595397723bd
Alan Ghobadi <aalan3@gmail.com>
2014-04-21 16:16:08 +0200

    Simple test script to filter out data

---
+++ scripts/scrape/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+
+# FILTERS
+
+# Remove empty rows
+def filter_empty(json_data):
+    output = []
+    for row in json_data:
+        if row['code'] != '':
+            output.append(row)
+    return output
+
+def group_by_peroid(json_data):
+    output = {}
+    period = None
+    for row in json_data:
+        if row['period'] != '':
+            period = row['period']
+
+        output.setdefault(period, []).append(row)
+    return output
+
+
+# Eat kimono-JSON and spit clean usable JSON
+with open("it1415.json") as json_file:
+    json_data = json.load(json_file)
+    filtered_data = filter_empty(json_data['collection1'])
+    #filtered_data = group_by_peroid(filtered_data)
+    pprint(filtered_data)
+
