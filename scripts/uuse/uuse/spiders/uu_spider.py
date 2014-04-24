06c8a46b2112db146deb49f416505ed7a74b470c
Alan Ghobadi <aalan3@gmail.com>
2014-04-24 10:34:30 +0200

    Fixed JSON output + group by period

--- scripts/uuse/uuse/spiders/uu_spider.py
+++ scripts/uuse/uuse/spiders/uu_spider.py
@@ -13,22 +13,37 @@ class UuSpider(Spider):
         sel = Selector(response)
         selrows = sel.xpath('//div[@class="articleText"]//table/tbody//tr')
         data = []
+        #output = {}
         for selrow in selrows:
             code = selrow.xpath('td[2]/p//text()').extract()
             if code != [] and code[0] != u'\u00a0' and code[0].find('1') == 0: # Is it really a course code?
+                period_extracted = self.empty_help(selrow.xpath('td[1]/p//text()').extract())
+                if period_extracted != '' and period_extracted != None:
+                    period = period_extracted
+                
                 row = UuItem()
-                row["period"] = self.empty_help(selrow.xpath('td[1]/p//text()').extract())
+                #row["period"] = self.empty_help(selrow.xpath('td[1]/p//text()').extract())
+                row["period"] = period
                 row["code"] = self.empty_help(code)
                 row["name"] = self.empty_help(selrow.xpath('td[3]/p//text()').extract())
                 row["credits"] = self.empty_help(selrow.xpath('td[4]/p//text()').extract())
                 row["level"] = self.empty_help(selrow.xpath('td[5]/p//text()').extract())
+                #output.setdefault(period, []).append(row)
                 data.append(row)
-        print data
+        #print output
+        #return output
         return data
                 
-        #filename = response.url.split("/")[-2]
-        #open(filename, 'wb').write(response.body)
-    
+    def group_by_period(self, data):
+        output = {}
+        period = None
+        for row in data:
+            if row['period'] != '' and row['period'] != None:
+                period = row['period']
+
+            output.setdefault(period, []).append(row)
+        return output
+
     def empty_help(self, arr):
         if arr != []:
             return arr[0].replace(u'\u00a0', '')
