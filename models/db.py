7fab7e3117eb76ff58d1fd0c6f44a47ea409ae7b
pofferfish <malin.lundberg91@gmail.com>
2014-04-29 16:04:13 +0200

    fungerande databas
    början på databas med typ hälften av tabelllerna. Fungerar att öppna i
    web2py-webbgränsnittet

--- models/db.py
+++ models/db.py
@@ -1,6 +1,49 @@
-db = DAL('mysql://username:password@localhost/test')
+db = DAL('sqlite://storage.db')
 
-db.define_table('Program'
-                db.Field('id'),
-                db.Field('namn'),
-                db.Field('beskrivning'))
+db.define_table('institution',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('institution_kursplan',
+                db.Field('institution', 'integer',notnull=True), #not null
+                db.Field('kursplan', 'integer',notnull=True)) #not null
+
+db.define_table('larare_kurstillfälle',
+                db.Field('larare', 'integer', notnull=True),  #not null
+                db.Field('kurstillfalle', 'integer', notnull=True)) #not null
+
+db.define_table('inriktning',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('samlasning',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('larare',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('niva',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('djup',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('program',
+                db.Field('namn', 'string', length=255,),
+                db.Field('beskrivning', 'string', length=1023))
+
+db.define_table('studieplan',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023),
+                db.Field('program', 'integer'))
+
+db.define_table('kursegenskap',
+                db.Field('namn', 'string', length=255),
+                db.Field('beskrivning', 'string', length=1023),
+                db.Field('varde', 'integer'),
+                db.Field('vardenamn', 'string', length=255),
+                db.Field('vardebeskrivning', 'string', length=1023))
