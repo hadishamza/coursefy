02fa90d111d31790a105db6a32f6d8cfbafcd702
Sofia Backman <sobackman@gmail.com>
2014-05-02 11:22:06 +0200

    Resten av databasen

--- models/db.py
+++ models/db.py
@@ -47,3 +47,73 @@ db.define_table('kursegenskap',
                 db.Field('varde', 'integer'),
                 db.Field('vardenamn', 'string', length=255),
                 db.Field('vardebeskrivning', 'string', length=1023))
+
+db.define_table('kursrelation', 
+                Field('namn', 'string', length = 255), 
+                Field('beskrivning', 'string', length = 1023))
+
+db.define_table('omradesklassning', 
+                Field('namn', 'string', length = 255), 
+                Field('beskrivning', 'string', lenght = 1023))
+
+db.define_table('betygsskala', 
+                Field('namn', 'string', lenght = 255), 
+                Field('beskrivning', 'string' lenght = 1023))
+
+db.define_table('perioder', 
+                Field('kurstillfalle', 'reference kurstillfalle'), 
+                Field('period', 'integer'))
+
+db.define_table('kursplan', 
+                Field('namn', 'string', lenght = 255), 
+                Field('poang', 'integer'), 
+                Field('kurskod', 'string', lenght = 255), 
+                Field('beslutsdatum', 'date'), 
+                Field('behorighet', 'string', lenght = 255), 
+                Field('niva', 'integer'), 
+                Field('betygsskala', 'integer'), 
+                Field('galler_fran', 'date'), 
+                Field('institution', 'integer'), 
+                Field('mal', 'string', lenght = 1023), 
+                Field('innehall', 'string', length = 1023), 
+                Field('undervisning', 'string', lenght = 1023), 
+                Field('examination', 'string', lenght = 1023), 
+                Field('kurslitteratur', 'string', lenght = 1023))
+
+db.define_table('kurstillfalle', 
+                Field('kursplan', 'integer'), 
+                Field('anmalningskod', 'string', lenght = 255), 
+                Field('fart', 'integer'), Field('larare', 'integer'))
+
+db.define_table('kurstillfalle_studieplan', 
+                Field('studieplan', 'reference studieplan'), 
+                Field('kurstillfalle', 'reference kurstillfalle'), 
+                Field('obligatorisk', 'integer'), 
+                Field('startperiod', 'integer'), 
+                Field('inriktning', 'refenrece inriktning'), 
+                Field('beskrivning', 'string', lenght = 1023))
+
+db.define_table('kursrelationer', 
+                Field('kursrelation', 'reference kursrelation'), 
+                Field('kalla', 'reference kurstillfalle'), 
+                Field('mal', 'reference kurstillfalle'), 
+                Field('dubbelinriktning', 'integer'))
+
+db.define_table('kursegenskaper', 
+                Field('kursegenskaper', 'reference kursegenskap'), 
+                Field('kurstillfalle', 'reference kurstillfalle'),
+                Field('varde', 'integer'))
+
+db.define_table('samlasningar', 
+                Field('samlasning', 'reference samlasning'), 
+                Field('program', 'reference program'), 
+                Field('kurstillfalle', 'reference kurstillfalle'))
+
+db.define_table('overlappning', 
+                Field('kursplan1', 'reference kursplan'), 
+                Field('kursplan2', 'reference kursplan'))
+
+db.define_table('omradesklassningar', 
+                Field('kursplan', 'reference kursplan'), 
+                Field('omradesklassning', 'omradesklassning'), 
+                Field('djup', 'reference djup'))
