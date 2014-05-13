3f481c1c3cfe3e74c60ae13279bbdbf2b4109494
Alan Ghobadi <aalan3@gmail.com>
2014-05-13 14:28:43 +0200

    Some general cleanup

--- scripts/uuse/uuse/spiders/uu_spider.py
+++ scripts/uuse/uuse/spiders/uu_spider.py
@@ -13,14 +13,13 @@ class UuSpider(Spider):
         sel = Selector(response)
         selrows = sel.xpath('//div[@class="articleText"]//table/tbody//tr')
         data = []
-        #output = {}
         for selrow in selrows:
             code = selrow.xpath('td[2]//text()').extract()
             if code != [] and code[0] != u'\u00a0' and (code[0].find('1') == 0 or code[0].find('2') == 0): # Is it really a course code?
                 period_extracted = self.empty_help(selrow.xpath('td[1]/p//text()').extract())
                 if period_extracted != '' and period_extracted != None:
                     period = period_extracted
-                
+
                 row = UuItem()
                 if peroid == "Alt.":
                     period = "34"
@@ -30,10 +29,7 @@ class UuSpider(Spider):
                 row["name"] = self.empty_help(selrow.xpath('td[3]/p//text()').extract())
                 row["credits"] = self.empty_help(selrow.xpath('td[4]/p//text()').extract())
                 row["level"] = self.empty_help(selrow.xpath('td[5]/p//text()').extract())
-                #output.setdefault(period, []).append(row)
                 data.append(row)
-        #print output
-        #return output
         return data
 
     def empty_help(self, arr):
