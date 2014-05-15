fd1e6ad3243d0fb76a3f2e9e403b013775b6a3f6
Sofia Backman <sobackman@gmail.com>
2014-05-15 17:01:18 +0200

    hämtning från soap

--- controllers/soap.py
+++ controllers/soap.py
@@ -11,11 +11,32 @@ def index():
     client.set_options(port="PlanTjanstHttpSoap11Endpoint")
 
     response = client.service.hamtaKursplanKurskod(kurskod="1MA025")
-    fordjukod1 = response['kurs']['huvudomradeFordjupningar'][2]#['fordjupningskod']#['huvudomrade']#['benamning']
-    namnfordju1 = response['kurs']['huvudomradeFordjupningar'][3][1]
+    namn = response['kurs']['namn']
+    poang = response['kurs']['poang']
+    fordjukod1 = response['kurs']['huvudomradeFordjupningar'][0]['fordjupningskod']#['fordjupningskod']#['huvudomrade']#['benamning']
+    amne1 = response['kurs']['huvudomradeFordjupningar'][0]['huvudomrade']['benamning']
+    fordjukod2 = ""
+    amne2 = ""
 
+#check om kursen innehåller två huvudämnen. if so så hämtar vi det andra huvudämnet
+    gotdata = 1
+    try:
+        gotdata = response['kurs']['huvudomradeFordjupningar'][1]
+    except IndexError:
+        gotdata = 0
+    if gotdata:
+        fordjukod2 = response['kurs']['huvudomradeFordjupningar'][1]['fordjupningskod']#['fordjupningskod']#['huvudomrade']#['benamning']
+        amne2 = response['kurs']['huvudomradeFordjupningar'][1]['huvudomrade']['benamning']
+    
+# lägg in allt i databasen!!!!
 
-    print result
+    print namn
+    print poang
+    print fordjukod1
+    print amne1
+    print fordjukod2
+    print amne2
 
-    return dict(message=result)
+    #return dict(message=(namn, poang, fordjukod1, amne1, fordjukod2, amne2))
+    #return dict(message=amne1)
 
