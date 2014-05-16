2490e726135bf64f1f498937188be9b5582ebf6f
Alan Ghobadi <aalan3@gmail.com>
2014-05-16 14:42:43 +0200

    Fixed period "43-44" problem

--- scripts/uuse/uuse/spiders/uu_spider.py
+++ scripts/uuse/uuse/spiders/uu_spider.py
@@ -19,16 +19,26 @@ class UuSpider(Spider):
             code = selrow.xpath('td[2]//text()').extract()
             period_extracted = self.empty_help(selrow.xpath('td[1]/p//text()').extract())
             period_not_empty = (period_extracted != None and period_extracted != '' and period_extracted != '.')
-            if period_not_empty or period_extracted.lower() == "alt.":
-                    period = period_extracted
+            if period_not_empty and period_extracted.lower() != "alt.":
+                period = period_extracted.replace(" ", "") #Get rid of whitespace
+
             if code != [] and code[0] != u'\u00a0' and len(code[0]) == 6: # Is it really a course code?
+                periods = period.split("-")
                 row = UuItem()
                 row["period"] = period
                 row["code"] = self.empty_help(code)
                 row["name"] = self.empty_help(selrow.xpath('td[3]/p//text()').extract())
                 row["credits"] = self.empty_help(selrow.xpath('td[4]/p//text()').extract())
                 row["level"] = self.empty_help(selrow.xpath('td[5]/p//text()').extract())
+
+                if len(periods) > 1:
+                    row["period"] = periods[0]
+                    row2 = row.copy()
+                    row2["period"] = periods[1]
+                    data.append(row2)
+
                 data.append(row)
+
         return data
 
     def empty_help(self, arr):
@@ -36,3 +46,4 @@ class UuSpider(Spider):
             return arr[0].replace(u'\u00a0', '')
         else:
             ""
+
