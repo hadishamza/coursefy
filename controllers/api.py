b5544385c8666384a4b19b3577cfdaa9ee7b354f
Alan Ghobadi <aalan3@gmail.com>
2014-05-19 11:32:05 +0200

    Convert indentation to spaces

--- controllers/api.py
+++ controllers/api.py
@@ -2,7 +2,7 @@
 def courses():
     response.view = 'generic.json'
     def GET(searchword):
-    	kurser = kursplan = db(db.kursplan.kurskod.contains(searchword)).select(limitby=(0,5))
+        kurser = kursplan = db(db.kursplan.kurskod.contains(searchword)).select(limitby=(0,5))
         return kurser.json()
     return locals()
 
